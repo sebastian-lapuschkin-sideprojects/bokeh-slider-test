d7c64f1df140861a5072e8bbf4037a11049c1f6d
Sebastian Lapuschkin <sebastian.lapuschkin@googlemail.com>
2019-09-18 14:03:21 +0200

    adding 30% an 32% levels to chart

--- bars.py
+++ bars.py
@@ -13,7 +13,7 @@ from bokeh.layouts import layout
 from bokeh.models import Button
 from bokeh.models import Slider
 from bokeh.models import Label
-from bokeh.palettes import Spectral4 as COLORMAP
+from bokeh.palettes import Spectral6 as COLORMAP
 from bokeh.models import ColumnDataSource
 from bokeh.models.ranges import Range1d
 
@@ -24,9 +24,12 @@ from bokeh.models.widgets.inputs import TextInput
 from bokeh.events import ButtonClick
 
 
-provisionsquellen = ['Andere ({}% v. Brutto) ', 'Eigemarkemarke ({}% v. Brutto)', 'Kombinert (E/A: {}%/{}%)', 'Neu (28% v. Netto)']
+provisionsquellen = ['Andere ({}% v. Brutto) ', 'Eigemarkemarke ({}% v. Brutto)', 'Kombinert (E/A: {}%/{}%)', 'Neu (28% v. Netto)', 'Neu (30% v. Netto)', 'Neu (32% v. Netto)']
 data_source = ColumnDataSource(data=dict(   provision=[],
-                                            provision_neu=[],
+                                            line_limits=[],
+                                            provision_neu_28=[],
+                                            provision_neu_30=[],
+                                            provision_neu_32=[],
                                             quellen=[],
                                             text=[],
                                             color=COLORMAP,
@@ -40,21 +43,27 @@ def set_data(umsatz=3000, anteil_hausmarke=66):
     provision_andere = umsatz*(0.2+bonus)
     provision_eigenmarke = umsatz*(0.23+bonus)
     provision_kombiniert = (anteil_hausmarke/100) * provision_eigenmarke + (100 - anteil_hausmarke)/100 * provision_andere
-    provision_neu = (umsatz / 1.19) * 0.28
+    provision_neu_28 = (umsatz / 1.19) * 0.28
+    provision_neu_30 = (umsatz / 1.19) * 0.30
+    provision_neu_32 = (umsatz / 1.19) * 0.32
 
     quellen = [provisionsquellen[0].format(int(100*(0.2+bonus))),
                provisionsquellen[1].format(int(100*(0.23+bonus))),
                provisionsquellen[2].format(anteil_hausmarke, 100-anteil_hausmarke),
-               provisionsquellen[3]]
-    provision = [provision_andere, provision_eigenmarke, provision_kombiniert, provision_neu]
+               provisionsquellen[3],
+               provisionsquellen[4],
+               provisionsquellen[5]]
+    provision = [provision_andere, provision_eigenmarke, provision_kombiniert, provision_neu_28, provision_neu_30, provision_neu_32]
     data_source.data = dict(provision=provision,
-                            line_limits=[0, 1, 2, len(provision)+1],
-                            provision_neu=[provision_neu]*4,
+                            line_limits=[0] + [len(provision)+1]*5,
+                            provision_neu_28=[provision_neu_28]*6,
+                            provision_neu_30=[provision_neu_30]*6,
+                            provision_neu_32=[provision_neu_32]*6,
                             quellen=quellen,
                             text=['{} €'.format(int(p)) for p in provision],
                             color=COLORMAP)
     plot.x_range.factors = quellen
-    plot.y_range = Range1d(0, 1.2*max(provision))
+    plot.y_range = Range1d(0, 1.4*max(provision))
 
 
 
@@ -64,10 +73,12 @@ plot = figure( x_range=provisionsquellen,
                title='Verkaufsprovision - Altes vs neues System',
                toolbar_location=None,
                tools='',
-               plot_width=800)
+               plot_width=1100)
 plot.vbar(x='quellen', top='provision', width=0.9, color='color', legend='quellen', source=data_source)
 plot.text(x='quellen', y='provision', text='text', text_align='center', text_baseline='top', source=data_source)
-plot.line(x='line_limits', y='provision_neu', line_color=COLORMAP[-1], source=data_source)
+plot.line(x='line_limits', y='provision_neu_28', line_color=COLORMAP[-3], source=data_source)
+plot.line(x='line_limits', y='provision_neu_30', line_color=COLORMAP[-2], source=data_source)
+plot.line(x='line_limits', y='provision_neu_32', line_color=COLORMAP[-1], source=data_source)
 plot.xgrid.grid_line_color = None
 plot.legend.orientation = 'horizontal'
 plot.legend.location='top_center'
@@ -79,7 +90,7 @@ set_data(UMSATZ, ANTEIL_HAUSMARKE)
 
 #add gui elements and callbacks
 anteil_slider = Slider(start=0, end=100, step=1, value=ANTEIL_HAUSMARKE, title='Anteil Eigenmarkeprodukte in %')
-umsatz_slider = Slider(start=0, end=3000, step=1, value=UMSATZ, title='Umsatz pro Monat in € (Brutto)')
+umsatz_slider = Slider(start=0, end=4000, step=1, value=UMSATZ, title='Umsatz pro Monat in € (Brutto)')
 anteil_textinput = TextInput(value=str(ANTEIL_HAUSMARKE))
 umsatz_textinput = TextInput(value=str(UMSATZ))
 bonus_label = Button(label='SET ME')
@@ -94,12 +105,18 @@ def set_label_data(umsatz, anteil):
         bonus_label.label = "Umsatz < 1750 €  : Kein Bonus"
         bonus_label.button_type = 'default'
 
-
-    if data_source.data['provision'][2] < data_source.data['provision'][3]:
-        provision_label.label = "Neue Provision fällt geringer aus!"
+    provision_kombiniert = data_source.data['provision'][2]
+    if provision_kombiniert > data_source.data['provision_neu_32'][0]:
+        provision_label.label = "Neue Provision (32%) fällt geringer aus!"
+        provision_label.button_type = 'danger'
+    elif provision_kombiniert > data_source.data['provision_neu_30'][0]:
+        provision_label.label = "Neue Provision (30%) fällt geringer aus!"
         provision_label.button_type = 'warning'
+    elif provision_kombiniert > data_source.data['provision_neu_28'][0]:
+        provision_label.label = "Neue Provision (28%) fällt geringer aus!"
+        provision_label.button_type = 'primary'
     else:
-        provision_label.label = "Neue Provision ist höher."
+        provision_label.label = "Neue Provision (28%) ist höher."
         provision_label.button_type = 'success'
 
 
